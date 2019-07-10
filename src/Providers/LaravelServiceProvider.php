6039cbe1ba5a0f2db24911f06aa8aa6bc5600271
agung3wi <agung3wi@users.noreply.github.com>
2019-07-10 10:17:06 +0700

    Update LaravelServiceProvider.php

--- src/Providers/LaravelServiceProvider.php
+++ src/Providers/LaravelServiceProvider.php
@@ -19,12 +19,13 @@ class LaravelServiceProvider extends ServiceProvider
      */
     public function register()
     {
+        
+        // use routes middleware
+        $this->app['router']->aliasMiddleware('phy.auth', \Phy\CommonApi\Middleware\ValidTokenUser::class);
+        
         if(env('USE_COMMON_ROUTES', true)){
             // load routes
             $this->loadRoutesFrom(__DIR__.'/../routes.php');
-
-            // use routes middleware
-            $this->app['router']->aliasMiddleware('Phy.auth', \Phy\CommonApi\Middleware\ValidTokenUser::class);
         }
     }
 }
