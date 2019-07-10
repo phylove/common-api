5153f2f90ca9a5ff90df8addffb40133fd7a837c
agung3wi <agung3wi@users.noreply.github.com>
2019-07-10 09:55:58 +0700

    Update LaravelServiceProvider.php

--- src/Providers/LaravelServiceProvider.php
+++ src/Providers/LaravelServiceProvider.php
@@ -19,16 +19,6 @@ class LaravelServiceProvider extends ServiceProvider
      */
     public function register()
     {
-
-        $menus = \App\Service\GetMenu::getInstance();
-        $menus->add(1, 'Home', url('/'), 'fa fa-dashboard', true, [
-            $menus->addSubMenu(1, 'Dashboard', url('/'), 'fa fa-circle', true)
-        ]);
-
-        $menus->add(2, 'User', url('/'), 'fa fa-user', true, [
-            $menus->addSubMenu(1, 'Manage User', url('/manage-user'), 'fa fa-circle', 'viewUser')
-        ]);
-
         if(env('USE_COMMON_ROUTES', true)){
             // load routes
             $this->loadRoutesFrom(__DIR__.'/../routes.php');
